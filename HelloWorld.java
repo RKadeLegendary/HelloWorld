3775bcdf80d58b82a0e8c93db7c6ed96a8d69af7
RKadeLegendary <rkade159@gmail.com>
2026-03-27 09:24:33 +0000

    Changed printMessage on conflict branch

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void printMessage() {
-        System.out.println("Hello World!");
+        System.out.println("Hello from the new branch!");
     }
 
     public static void main(String[] args) {
