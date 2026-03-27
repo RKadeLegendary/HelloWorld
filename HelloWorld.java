96b82f0fbca8c589fdec6af40dd3d81fc06df92d
RKadeLegendary <rkade159@gmail.com>
2026-03-27 09:23:27 +0000

    Added 'printMessage' method

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,9 @@
 public class HelloWorld {
-    public static void main(String[] args) {
+    public static void printMessage() {
         System.out.println("Hello World!");
+    }
+
+    public static void main(String[] args) {
+        printMessage();
     }
 }
